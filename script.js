af8c3afad43f9099be6da39403a67bcf7d185595
git504 <git504@mailo.com>
2021-05-18 11:10:16 +0200

    Inputs

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // 2 Inputs
+//on recupere et on stocke
 
 function ecouteinput () {
     let nbr1 = document.getElementsById("enternbrs1").value;
     let nbr2 = document.getElementsById("enternbrs2").value;
     console.log("Hello you, here is " + nbr1 + " and " + nbr2);
-}
+} 
 
 
 // Opérande 
