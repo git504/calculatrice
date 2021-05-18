8625f75c9cf08bea461bb4d52b21941e05b6535d
git504 <git504@mailo.com>
2021-05-18 15:13:29 +0200

    Inputs

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ function calculer() {
     let nbr2 = document.getElementById("enternbrs2").value;
     console.log("Hello you, here is " + nbr1 + " and " + nbr2);
 
-    if(typeof parseInt(nbr1) === 'number' && typeof parseInt(nbr2) === 'number') {
-            console.log("Tout va bien ;-) C'est un nombre !");
-            }else{
-            console.log(typeof nbr1);
-            }
-} 
+        if(typeof parseInt(nbr1) === 'number' && typeof parseInt(nbr2) === 'number') {
+        console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");
+        } else if (typeof nbr1 === 'string') {
+        console.log(nbr1 + " entrer un nombre");
+        } else if (typeof nbr2 === 'string') {
+            console.log(nbr2 + " entrer un nombre");
+        }
 
+        // } else {
+        // console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");
+        // }
+} 
 
 
 // Opérande 
