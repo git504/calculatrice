505b544a0cec80cad897b92eda641567473ca971
git504 <git504@mailo.com>
2021-05-18 15:41:46 +0200

    Inputs

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // 2 Inputs
-// on recupere et on stocke
-// on securise avec le HTML input REQUIRED et MIN
 
 function calculer() {
     let nbr1 = document.getElementById("enternbrs1").value;
     let nbr2 = document.getElementById("enternbrs2").value;
-    console.log("Hello you, here is " + nbr1 + " and " + nbr2);
+    // console.log("Hello you, here is " + nbr1 + " and " + nbr2);
+
 
         if((typeof parseInt(nbr1) === 'number') && (typeof parseInt(nbr2) === 'number')) {
         console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");
-        } else if ((typeof parseInt(nbr1) !== 'number') && (typeof parseInt(nbr2) !== 'number')) {
+            } else if ((typeof parseInt(nbr1) !== 'number') && (typeof parseInt(nbr2) !== 'number')) {
         console.log("entrer un nombre");
-        } else {
+            } else {
         }
 
         if(document.getElementById("add").selected) {
