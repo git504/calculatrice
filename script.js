49684c56f3d16dec9ea3942ee03c8794b57a9b2b
git504 <git504@mailo.com>
2021-05-18 11:35:38 +0200

    Inputs

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // 2 Inputs
+
+
 //on recupere et on stocke
 
 function calculer() {
     let nbr1 = document.getElementById("enternbrs1").value;
     let nbr2 = document.getElementById("enternbrs2").value;
     console.log("Hello you, here is " + nbr1 + " and " + nbr2);
+
+    if(typeof nbr1 === 'number' && typeof nbr2 === 'number') {
+            console.log("Everything is Ok");
+            }
+            console.log("Enter a number");
 } 
 
 
