5efe0729bcdd1c2c2a398381059ca3ddad8152f8
git504 <git504@mailo.com>
2021-05-18 15:53:28 +0200

    Inputs

--- script.js
+++ script.js
@@ -5,31 +5,33 @@ function calculer() {
     let nbr2 = document.getElementById("enternbrs2").value;
     // console.log("Hello you, here is " + nbr1 + " and " + nbr2);
 
-
-        if((typeof parseInt(nbr1) === 'number') && (typeof parseInt(nbr2) === 'number')) {
-        console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");}
-        else if ((typeof parseInt(nbr1) !== 'number') && (typeof parseInt(nbr2) !== 'number')) 
-        {console.log("entrer un nombre");} 
-        else {console.log("hello")}
+    let resultat = parseInt(nbr1) + parseInt(nbr2)
+    //test NaN
 
 
 
-        if(document.getElementById("add").selected) {
-            resultat = parseInt(nb1) + parseInt(nbr2);
-        }
-        else if(document.getElementById("sous").selected) {
-            resultat = parseInt(nb1) - parseInt(nbr2);
-        }
-        else if(document.getElementById("mult").selected) {
-            resultat = parseInt(nb1) * parseInt(nbr2);
-        }
+        if(resultat !== NaN) {
+        alert(resultat);}
         else {
-            resultat = parseInt(nbr1) / parseInt(nbr2);
-        }
+        alert("Ouille ! ;-( Entrer un nombre.")} 
+        //Qd on recupere valeur ds input par defaut = string. Si parsInt = number
 
-        document.getElementById("btn").onclick = calculer();
+
+        // if(document.getElementById("add").selected) {
+        //     resultat = parseInt(nb1) + parseInt(nbr2);
+        // }
+        // else if(document.getElementById("sous").selected) {
+        //     resultat = parseInt(nb1) - parseInt(nbr2);
+        // }
+        // else if(document.getElementById("mult").selected) {
+        //     resultat = parseInt(nb1) * parseInt(nbr2);
+        // }
+        // else {
+        //     resultat = parseInt(nbr1) / parseInt(nbr2);
+        // }
+
+        // document.getElementById("btn").onclick = calculer();
 } 
-
 
 
 // Opérande 
