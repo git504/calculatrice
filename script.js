c387d4a9c3ae4116e29ba31f7a4d149c65c73821
git504 <git504@mailo.com>
2021-05-18 15:36:14 +0200

    Inputs

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // 2 Inputs
-
 // on recupere et on stocke
 // on securise avec le HTML input REQUIRED et MIN
 
@@ -8,25 +7,40 @@ function calculer() {
     let nbr2 = document.getElementById("enternbrs2").value;
     console.log("Hello you, here is " + nbr1 + " and " + nbr2);
 
-        if(typeof parseInt(nbr1) === 'number' && typeof parseInt(nbr2) === 'number') {
+        if((typeof parseInt(nbr1) === 'number') && (typeof parseInt(nbr2) === 'number')) {
         console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");
-        } else if (typeof nbr1 === 'string') {
-        console.log(nbr1 + " entrer un nombre");
-        } else if (typeof nbr2 === 'string') {
-            console.log(nbr2 + " entrer un nombre");
+        } else if ((typeof parseInt(nbr1) == 'number') && (typeof parseInt(nbr2) == 'number')) {
+        console.log("entrer un nombre");
+        } else {
         }
 
-        // } else {
-        // console.log("Tout va bien ;-) " + nbr1 + " et " + nbr2 + " sont des nombres !");
-        // }
+        if(document.getElementById("add").selected) {
+            resultat = parseInt(nb1) + parseInt(nbr2);
+        }
+        else if(document.getElementById("sous").selected) {
+            resultat = parseInt(nb1) - parseInt(nbr2);
+        }
+        else if(document.getElementById("mult").selected) {
+            resultat = parseInt(nb1) * parseInt(nbr2);
+        }
+        else {
+            resultat = parseInt(nbr1) / parseInt(nbr2);
+        }
+
+        document.getElementById("btn").onclick = calculer();
 } 
+
 
 
 // Opérande 
 
 
 
+
+
 // Calculer
+
+
 
 
 
