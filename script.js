2d352dc43b427141cc7dc3163c1e7b6bfa61e0fb
git504 <git504@mailo.com>
2021-05-18 10:34:17 +0200

    input

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//selcteur d'operateur
+//selecteur d'operateur
 
 var operateur = document.getElementsByName("operateur")
 function selecteuroperateur() {
 
 }
-operateur.addEventlistener("click", selecteuroperateur)
+operateur.addEventlistener("click", function(e))
 
 
 //calculer
+
 valeur1 = document.getElementsByName("enternbrs1").value;
 valeur2 = document.getElementsByName("enternbrs2").value;
+
